ebf15f47e996662d930b6de1ec64cdf6d2f7b210
nikitos03 <83069842+nikitos03@users.noreply.github.com>
2023-01-24 18:16:29 +0300

    Add files via upload

---
+++ User/checkUser.cs
@@ -0,0 +1,26 @@
+﻿namespace Banking.UI
+{
+    public class checkUser
+    {
+
+        /// <summary>
+        /// get - получает значения свойства;
+        /// set - устанавливает значение свойства;
+        /// IsAdmin - логическая переменная;
+        /// Status - елси true(1), то это Admin, если false(0), то это User;
+        /// В chtckUser передается строка с логином и логической переменной;
+        /// 
+        /// </summary>
+
+        public string Login { get; set; }
+        public bool IsAdmin { get; }
+
+        public string Status => IsAdmin ? "Admin" : "User";
+
+        public checkUser(string loginUser, bool isAdmin)
+        {
+            Login = loginUser;
+            IsAdmin = isAdmin;
+        }
+    }
+}
